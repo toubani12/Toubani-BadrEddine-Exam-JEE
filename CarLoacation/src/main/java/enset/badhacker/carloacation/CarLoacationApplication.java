a6526fd87c37fafafbd7e1889636a288aab997bb
badr <fadre6@gmail.com>
2026-05-11 14:20:55 +0100

    first upload

---
+++ CarLoacation/src/main/java/enset/badhacker/carloacation/CarLoacationApplication.java
@@ -0,0 +1,13 @@
+package enset.badhacker.carloacation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLoacationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarLoacationApplication.class, args);
+    }
+
+}
---
+++ CarLoacation/src/test/java/enset/badhacker/carloacation/CarLoacationApplicationTests.java
@@ -0,0 +1,13 @@
+package enset.badhacker.carloacation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarLoacationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
